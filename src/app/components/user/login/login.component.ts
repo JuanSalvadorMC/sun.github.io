d675a604fb4b204b8b5d319287ce860a14b3dbf3
MikeBucio <54648802+MikeBucio@users.noreply.github.com>
2020-08-18 12:05:24 -0500

    Recuperar contraseña

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -12,6 +13,7 @@ import { NotificacionesService } from '../../../services/notificaciones.service'
 import { MatDialog } from '@angular/material/dialog';
 import { DatosRegistroRedSocialComponent } from '../../modals/datos-registro-red-social/datos-registro-red-social.component';
 import { NavbarService } from '../../../services/navbar.service';
+import { RecuperarContraseniaComponent } from '../../modals/recuperar-contrasenia/recuperar-contrasenia.component';
 
 
 @Component({
@@ -128,4 +130,13 @@ export class LoginComponent implements OnInit {
       
     });
   }
+
+  openModarRecuperarContra(){
+    const dialogRef = this.dialog.open(RecuperarContraseniaComponent, {
+      width: '350px',
+      height: '350px',
+      data: { id : localStorage.getItem('idusu') }
+    });
+  }
+  
 }
