7cecaa69c9d19276999121782ba1e84bb901b798
MikeBucio <54648802+MikeBucio@users.noreply.github.com>
2020-08-22 13:53:24 -0500

    Homologación de etiquetas

--- src/app/components/resultComplete/contacto-equipamiento/contacto-equipamiento.component.ts
+++ src/app/components/resultComplete/contacto-equipamiento/contacto-equipamiento.component.ts
@@ -59,7 +59,7 @@ export class ContactoEquipamientoComponent implements OnInit {
           this.mostrarDatosContacto = true;
         }
         else if(resp.exito == false){
-          this.notificacionesService.confirmarAccion('Ya no cuentas con créditos disponibles para solicitar contacto','Ocurrió un error', 'Ir a Membrsías', 'Cancelar', 'warning').then(confirm=>
+          this.notificacionesService.confirmarAccion('No cuentas con créditos disponibles para solicitar contacto','Ocurrió un error', 'Ir a Membrsías', 'Cancelar', 'warning').then(confirm=>
           confirm.isConfirmed == true ? this.router.navigateByUrl('/membership'): false
           )}
       })
