8754a0220b1885d48b4b76d90ffbfdfe7ac80844
jaredDelao <jared.hernandez@ies-systems.com>
2020-08-23 21:44:25 -0500

    ajuste cors

--- src/app/services/esatdos.service.ts
+++ src/app/services/esatdos.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
@@ -7,29 +7,31 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class EsatdosService {
-  url='https://api-sepomex.hckdrk.mx/query/'
+  url='https://api-sepomex.hckdrk.mx/query/';
 
   estados:any[]=[];
+  headers = new HttpHeaders({
+    'skip': 'true' });
   constructor( private http: HttpClient) { }
 
 
   obtenerEstados(): Observable<any> {
-    return this.http.get(this.url+'get_estados').pipe(map((resp:any) => resp));
+    return this.http.get(this.url+'get_estados', {headers: this.headers}).pipe(map((resp:any) => resp));
   }
 
   obtenerMunicipios(estado):Observable<any> {
-    return this.http.get(this.url+`get_municipio_por_estado/${estado}`).pipe(map(resp =>resp))
+    return this.http.get(this.url+`get_municipio_por_estado/${estado}`, {headers: this.headers}).pipe(map(resp =>resp))
   }
 
   obtenerColoniaPorCP(municipio){
-    return this.http.get(this.url+ `get_colonia_por_cp/${municipio}`)
+    return this.http.get(this.url+ `get_colonia_por_cp/${municipio}`, {headers: this.headers})
   }
 
   obtenerInfoPorCP(cp){
-    return this.http.get(this.url+`info_cp/${cp}?type=simplified`)
+    return this.http.get(this.url+`info_cp/${cp}?type=simplified`, {headers: this.headers})
   }
   obtenerCodigoPostarMunicipio(cp){
-    return this.http.get(this.url+`get_cp_por_municipio/${cp}`)
+    return this.http.get(this.url+`get_cp_por_municipio/${cp}`, {headers: this.headers})
   }
   
 } 
