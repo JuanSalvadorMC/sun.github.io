6b987a371ffeaeaf41b3dd3d8c23b18b6fe3e046
MikeBucio <54648802+MikeBucio@users.noreply.github.com>
2020-09-30 14:15:51 -0500

    transición

--- src/app/components/esp/home/home.component.ts
+++ src/app/components/esp/home/home.component.ts
@@ -20,6 +20,43 @@ export class HomeComponent implements OnInit {
       this.ocultarRegistro = false;
      
     }
+  /*   var extension = {
+      cycle: function (e, extra) {
+        e || (this.paused = false)
+    
+        this.interval && clearTimeout(this.interval)
+    
+        var nextInterval;
+        var $active    = this.$element.find('carousel-item.active')
+        if (!extra) {
+          nextInterval = $active.data("duration") || this.options.interval;
+        } else {
+          var $next    = this.getItemForDirection('next', $active)
+          nextInterval = $next.data("duration") || this.options.interval;
+        }
+    
+        !this.paused
+          && (this.interval = setTimeout($.proxy(this.nextProxy, this), nextInterval))
+    
+        return this
+      }, 
+      pause: function (e) { 
+        e || (this.paused = true)
+    
+        if (this.$element.find('.next, .prev').length && $.support.transition) {
+          this.$element.trigger($.support.transition.end)
+          this.cycle(true)
+        }
+        this.interval = clearTimeout(this.interval)
+    
+        return this
+      },
+      nextProxy: function() {
+        this.next()
+        this.cycle(true, true)
+      }
+    } */
+    
   }
 
 }
