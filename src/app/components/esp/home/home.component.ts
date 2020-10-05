b17ab7d40c61d6d4b4ed79529a95c2312f5b7648
Salvador M.Cervantes <47789943+JuanSalvadorMC@users.noreply.github.com>
2020-10-05 11:54:04 -0500

    m

--- src/app/components/esp/home/home.component.ts
+++ src/app/components/esp/home/home.component.ts
@@ -9,7 +9,7 @@ import { map, repeatWhen, takeUntil } from 'rxjs/operators';
  
 })
 
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements OnInit {
 
   bgColor = 'primary';
   color = 'warn';
@@ -30,42 +30,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
       this.ocultarRegistro = false;
      
     }
-  /*   var extension = {
-      cycle: function (e, extra) {
-        e || (this.paused = false)
-    
-        this.interval && clearTimeout(this.interval)
-    
-        var nextInterval;
-        var $active    = this.$element.find('carousel-item.active')
-        if (!extra) {
-          nextInterval = $active.data("duration") || this.options.interval;
-        } else {
-          var $next    = this.getItemForDirection('next', $active)
-          nextInterval = $next.data("duration") || this.options.interval;
-        }
-    
-        !this.paused
-          && (this.interval = setTimeout($.proxy(this.nextProxy, this), nextInterval))
-    
-        return this
-      }, 
-      pause: function (e) { 
-        e || (this.paused = true)
-    
-        if (this.$element.find('.next, .prev').length && $.support.transition) {
-          this.$element.trigger($.support.transition.end)
-          this.cycle(true)
-        }
-        this.interval = clearTimeout(this.interval)
-    
-        return this
-      },
-      nextProxy: function() {
-        this.next()
-        this.cycle(true, true)
-      }
-    } */
+  
     
   }
 
