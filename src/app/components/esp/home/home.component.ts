14e8f8707a8ed0080c0d63a02c391779a5aa32c2
Salvador M.Cervantes <47789943+JuanSalvadorMC@users.noreply.github.com>
2020-10-06 15:37:22 -0500

    chingo de cambios en home carrusel responsive movile

--- src/app/components/esp/home/home.component.ts
+++ src/app/components/esp/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
 
    // SLIDER CONFIGURACION
    indice: number = 1;
-   velocidad: number = 10000;
+   velocidad: number = 15000;
  
    $interval = interval(6000);
    private readonly _stop = new Subject<void>();
